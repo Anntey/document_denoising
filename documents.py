750bfd146c8e17e22f0dc3288e48611810b35cfc
Anntey <51882759+Anntey@users.noreply.github.com>
2019-09-01 16:41:22 +0300

    Update documents.py

--- documents.py
+++ documents.py
@@ -1,11 +1,4 @@
 
-import tensorflow as tf
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
-config = ConfigProto()
-config.gpu_options.allow_growth = True
-session = InteractiveSession(config = config)
-
 #############
 # Libraries #
 #############
